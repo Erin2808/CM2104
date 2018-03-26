17b62bb2af8bea9dd517942061678c38d380dc6f
Erin Watson <erin@MC44296.rgu.ac.uk>
2018-03-26 15:40:37 +0100

    fixed?

--- lab15/server.js
+++ lab15/server.js
@@ -1,13 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/star_wars_quotes";
-const experess = require('express');
+const express = require('express');
 const app = express();
 
 app.use(express.static('public'))
 
 var db;
 
-MongoClient.connect(url, funciton(err, database){
+MongoClient.connect(url, function(err, database){
   if(err) throw err;
   db = database;
   app.listen(8080);
